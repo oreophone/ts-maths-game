a2bdb7039e69f3c02ea36a36eb768b7d3ca060d0
Adrian Carpio <u7418332@anu.edu.au>
2024-07-09 17:22:52 +1000

    question, questiontemplate started

---
+++ ts/QuestionTemplate.ts
@@ -0,0 +1,132 @@
+export const OPERATORS = [
+    'add',
+    'subtract',
+    'multiply',
+    'divide',
+    'square',
+    'cube',
+    'add3',
+    'sub3',
+    'addsubtract',
+    'subtractadd',
+] as const;
+
+export type operator = typeof OPERATORS[number];
+
+export type numberRange = [number, number];
+export type operatorTemplates = {
+    [o in operator]: numberRange | null
+}
+
+export type questionNumbers
+    = [number]
+    | [number, number]
+    | [number, number, number]
+    ;
+
+export class QuestionTemplate {
+    operatorTemplate: operatorTemplates;
+    isNegativeNumbersAllowed: boolean;
+
+    constructor(
+        params: {
+            [op in operator]?: numberRange
+        },
+        isNegativeNumbersAllowed: boolean = true
+    ) {
+
+        let questionTemplate = params;
+        this.operatorTemplate = <operatorTemplates>Object.fromEntries(OPERATORS.map((op) => [op, null]));
+        OPERATORS.forEach(op => {
+            let curValue = questionTemplate[op]
+            this.operatorTemplate[op] = curValue === undefined ?
+                null : curValue
+        });
+
+        this.isNegativeNumbersAllowed = isNegativeNumbersAllowed;
+    }
+
+    getAllowedOperations(): operator[] {
+        let opList: operator[] = OPERATORS.map((op) => op);
+        return opList.filter((op) => this.operatorTemplate[op] !== null);
+    }
+
+    fromBaseDifficulty(dif: number): QuestionTemplate {
+        switch (dif) {
+            case 1: return this.DIF1()
+            case 2: return this.DIF2()
+            case 3: return this.DIF3()
+            case 4: return this.DIF4()
+            case 5: return this.DIF5()
+            default: return this.DIF1()
+        }
+    }
+
+    private DIF1(): QuestionTemplate {
+        return new QuestionTemplate({
+            add: [0,10],
+            subtract: [0,10],
+            multiply: [0,5],
+            divide: [1,5],
+        },
+        /* isNegativeNumbersAllowed: */ false,
+        )
+    }
+
+    private DIF2(): QuestionTemplate {
+        return new QuestionTemplate({
+            add: [1,30],
+            subtract: [1,30],
+            multiply: [1,10],
+            divide: [1,10],
+            add3: [1,10],
+            sub3: [1,10],
+        },
+        /* isNegativeNumbersAllowed: */ false,
+        )
+    }
+
+    private DIF3(): QuestionTemplate {
+        return new QuestionTemplate({
+            add: [10,100],
+            subtract: [30,100],
+            multiply: [2,15],
+            divide: [2,15],
+            add3: [1,30],
+            sub3: [1,30],
+            addsubtract: [1,30],
+            subtractadd: [1,30],
+            square: [1,10],
+        })
+    }
+
+    private DIF4(): QuestionTemplate {
+        return new QuestionTemplate({
+            add: [10,500],
+            subtract: [10,500],
+            multiply: [2,40],
+            divide: [2,40],
+            add3: [10,100],
+            sub3: [10,100],
+            addsubtract: [10,100],
+            subtractadd: [10,100],
+            square: [2,20],
+            cube: [1,10],
+        })
+    }
+
+    private DIF5(): QuestionTemplate {
+        return new QuestionTemplate({
+            add: [100,999],
+            subtract: [100,999],
+            multiply: [2,99],
+            divide: [2,99],
+            add3: [10,500],
+            sub3: [10,500],
+            addsubtract: [10,500],
+            subtractadd: [10,500],
+            square: [2,50],
+            cube: [2,25],
+        })
+    }
+}
---
+++ ts/question.ts
@@ -0,0 +1,48 @@
+import {QuestionTemplate, operator, numberRange, questionNumbers, OPERATORS} from "./QuestionTemplate";
+
+export class Question {
+    questionNumbers: questionNumbers;
+    operator: operator;
+    private randomSeed: number | null;
+    private solution: number;
+
+
+    constructor(
+        questionNumbers: questionNumbers,
+        operator: operator,
+        randomSeed: number | null = null
+    ) {
+        this.questionNumbers = questionNumbers;
+        this.operator = operator;
+        this.randomSeed = randomSeed;
+        this.solution = this.generateSolution();
+    }
+
+    private generateSolution(): number {
+        switch ([this.operator, this.questionNumbers.length]) {
+            case ["add", 2]:
+                return this.questionNumbers[0] + this.questionNumbers[1]!
+            case ["subtract", 2]:
+                return this.questionNumbers[0] - this.questionNumbers[1]!
+            case ["multiply", 2]:
+                return this.questionNumbers[0] * this.questionNumbers[1]!
+            case ["divide", 2]:
+                // all generated divide questions must be of [n, cn], n, c natural (answer c)
+                return Math.floor(this.questionNumbers[0] + this.questionNumbers[1]!)
+            case ["square", 1]:
+                return Math.pow(this.questionNumbers[0], 2)
+            case ["cube", 1]:
+                return Math.pow(this.questionNumbers[0], 3)
+            case ["add3", 3]:
+                return this.questionNumbers[0] + this.questionNumbers[1]! + this.questionNumbers[2]!
+            case ["sub3", 3]: 
+                return this.questionNumbers[0] - this.questionNumbers[1]! - this.questionNumbers[2]!
+            case ["addsubtract", 3]:
+                // evaluated left->right
+                return this.questionNumbers[0] + this.questionNumbers[1]! - this.questionNumbers[2]!
+            case ["subtractadd", 3]:
+                return this.questionNumbers[0] - this.questionNumbers[1]! + this.questionNumbers[2]!
+            default: return Number.POSITIVE_INFINITY
+        }
+    }
+}
